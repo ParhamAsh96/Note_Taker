76c5e298dd6ae7556a086153249282fa342d39a9
Ali Parham Ashrafzadeh <parham.jobb@gmail.com>
2025-11-06 20:33:20 +0100

    Add upload voice to bucket function

--- src/notetaker/core/google_storage/voice_saver.py
+++ src/notetaker/core/google_storage/voice_saver.py
@@ -8,25 +8,41 @@ PROJECT_ID = "notetaker-477400"
 BUCKET_NAME = "voice-data-bucket-12345"
 LOCATION = "EU"
 
-client = storage.Client(project=PROJECT_ID)
+storage_client = storage.Client(project=PROJECT_ID)
 
-def get_or_create_bucket(name: str, location: str):
+def get_or_create_bucket(name, location):
     try:
-        bucket = client.get_bucket(name)
+        bucket = storage_client.get_bucket(name)
         print(f"Bucket already exists. Location: {bucket.location}")
         return bucket
+    
     except NotFound:
         pass
 
+
     try:
-        bucket = client.create_bucket(name, location=location)
+        bucket = storage_client.create_bucket(name, location=location)
         print(f"Created bucket: {bucket.name} in {bucket.location}")
         return bucket
+    
     except Conflict:
-        # Very rare race condition / retry case
-        bucket = client.get_bucket(name)
+        bucket = storage_client.get_bucket(name)
         print(f"Bucket exists after conflict. Location: {bucket.location}")
         return bucket
 
 bucket = get_or_create_bucket(BUCKET_NAME, LOCATION)
 
+
+def upload_to_bucket(blob_name, file_path, bucket_name):
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.upload_from_filename(file_path)
+        return True
+   
+    except Exception as e:
+        print(f"Upload failed: {e}")
+        return False
+    
+file_path = r"src/notetaker/core/data/output.wav"
+upload_to_bucket("lecture_voice", file_path, bucket)
