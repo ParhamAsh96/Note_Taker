b409128a7b46ee9adcc78e61ca3943019d81b8f3
Ali Parham Ashrafzadeh <parham.jobb@gmail.com>
2025-11-06 21:31:42 +0100

    Fix small bugs

--- src/notetaker/core/speech_to_text/google_stt.py
+++ src/notetaker/core/speech_to_text/google_stt.py
@@ -2,7 +2,7 @@ from google.oauth2 import service_account
 from google.cloud import speech
 
 
-def transcribe_vioce():
+def transcribe_voice():
     client_file = "src/notetaker/core/speech_to_text/stt_API.json"
     credentials = service_account.Credentials.from_service_account_file(client_file)
     client = speech.SpeechClient(credentials=credentials)
---
+++ src/notetaker/core/transcript_saver/transcript_saver.py
@@ -0,0 +1,4 @@
+from notetaker.core.speech_to_text.google_stt import transcribe_voice
+
+full_text = transcribe_voice()
+
