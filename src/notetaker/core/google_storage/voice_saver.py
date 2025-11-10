bf7cf00f622b8138792c0e111025129f8cf1913f
Ali Parham Ashrafzadeh <parham.jobb@gmail.com>
2025-11-10 06:21:51 +0100

    Add HOL blocking method

--- src/notetaker/core/google_storage/voice_saver.py
+++ src/notetaker/core/google_storage/voice_saver.py
@@ -1,4 +1,5 @@
 import os
+import math
 from google.cloud import storage
 from google.api_core.exceptions import NotFound, Conflict
 
@@ -9,6 +10,11 @@ BUCKET_NAME = "voice-data-bucket-12345"
 LOCATION = "EU"
 
 storage_client = storage.Client(project=PROJECT_ID)
+
+target_chunks = 64
+min_chunk_mb = 8
+max_chunk_mb = 64
+
 
 def get_or_create_bucket(name, location):
     try:
@@ -30,6 +36,26 @@ def get_or_create_bucket(name, location):
         return bucket
 
 
+def round_up_256kb(n_bytes):
+    base = 256 * 1024
+    return math.ceil(n_bytes / base) * base
+
+
+def pick_chunk_size(voice_path, target_chunks, min_chunk_mb, max_chunk_mb):
+    size = os.path.getsize(voice_path)
+    
+    if size == 0:
+        return min_chunk_mb * 1024 * 1024
+
+    min_chunk = min_chunk_mb * 1024 * 1024
+    max_chunk = max_chunk_mb * 1024 * 1024
+
+    ideal = max(size // max(1, target_chunks), min_chunk)
+    ideal = min(ideal, max_chunk)
+
+    return round_up_256kb(ideal)
+
+
 def upload_to_bucket(blob_name, file_path, bucket_name):
     try:
         bucket = storage_client.get_bucket(bucket_name)
@@ -47,4 +73,5 @@ def uplaod_voice():
     bucket = get_or_create_bucket(BUCKET_NAME, LOCATION)
 
     voice_path = r"src/notetaker/core/data/voice.wav"
+    pick_chunk_size(voice_path, target_chunks, min_chunk_mb, max_chunk_mb)
     upload_to_bucket("lecture_voice", voice_path, bucket)
