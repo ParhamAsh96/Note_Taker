593a46d06664ebbcb1f9bd61be85e8dfd14f494d
Ali Parham Ashrafzadeh <parham.jobb@gmail.com>
2025-11-10 06:28:01 +0100

    Update formatting

--- src/notetaker/core/cleaner/cleaner.py
+++ src/notetaker/core/cleaner/cleaner.py
@@ -7,14 +7,14 @@ def clean_data():
         "summary": "src/notetaker/core/data/summary.json",
     }
 
-    print("Removing old files...\n")
+    print("Removing old files...")
     for name, path in paths.items():
         try:
             os.remove(path)
-            print(f"The {name} file was deleted successfully.\n")
+            print(f"The {name} file was deleted successfully.")
 
         except FileNotFoundError:
             print(f"The {name} file was not found.\n")
         
         except Exception as e:
-            print(f"Something went wrong: {e}")
+            print(f"Something went wrong: {e}\n")
